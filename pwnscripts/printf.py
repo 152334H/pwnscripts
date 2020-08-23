b8f4eac7600d1b01803f439b12e7d3c9cdd59633
152334H <54623771+152334H@users.noreply.github.com>
2020-08-23 11:48:31 +0800

    avoid whitespace chars as far as possible

--- pwnscripts/printf.py
+++ pwnscripts/printf.py
@@ -71,7 +71,7 @@ def find_printf_offset_canary(resp) -> int:
 # New: helpers for leaking
 def leak_printf_deref_payload(buffer_offset: int, addr: list):
     extra = (len(addr)*(3+len(str(buffer_offset))+1)//(context.bits//8)) + 1
-    payload = ','.join("%{}$s".format(i) for i in range(buffer_offset+extra, buffer_offset+extra+len(addr))).ljust(extra*context.bits//8).encode()
+    payload = ','.join("%{}$s".format(i) for i in range(buffer_offset+extra, buffer_offset+extra+len(addr))).ljust(extra*context.bits//8,'\x19').encode()
     payload += b''.join(map(pack[context.arch], addr))
     return payload
 

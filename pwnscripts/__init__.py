e7abc6b16b18949a37a525ef5bf158e3852f633d
152334H <54623771+152334H@users.noreply.github.com>
2020-09-08 13:02:33 +0800

    add a new ROP helper; retire printf_old

--- pwnscripts/__init__.py
+++ pwnscripts/__init__.py
@@ -1,5 +1,7 @@
 '''A simple QoL module to make doing repetitive pwn easier'''
-from pwnscripts.printf_old import *
+from pwn import *
+from pwnscripts.string_checks import *
 from pwnscripts.libcdb_query import *
 from pwnscripts.uncommon import *
+from pwnscripts.rop import *
 import pwnscripts.fsb as fsb

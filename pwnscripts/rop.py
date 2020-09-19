9d3dcd7a4aaf2f10fcf878cd7203a469259d5b58
152334H <54623771+152334H@users.noreply.github.com>
2020-09-19 12:23:56 +0800

    add ret= argument to ROP.system_call

--- pwnscripts/rop.py
+++ pwnscripts/rop.py
@@ -1,4 +1,6 @@
 '''An extension of pwnlib.rop.rop.ROP.'''
+from pkg_resources import parse_version
+from pwnlib import __version__ as PWNLIB_VER
 from pwnlib import rop
 from pwnlib.rop.call import Call
 from pwnlib.util.packing import flat
@@ -15,7 +17,7 @@ class ROP(rop.rop.ROP):
         '''Dump the ROP chain in an easy-to-read manner
         Optional `base` argument to change the ROP base'''
         return self.build(base=base).dump()
-    def system_call(self, num: int, args: list):
+    def system_call(self, num: int, args: list, ret: bool=False):
         '''Making system calls without the massive overhead of SIGROP
         >>> context.arch = 'amd64'
         >>> r = ROP('./binary')
@@ -30,13 +32,31 @@ class ROP(rop.rop.ROP):
         0x0030:             0x40 [arg0] rdi = AppendedArgument(['/bin/sh'], 0x0)
         0x0038:         0x4022b4 syscall
         0x0040:   b'/bin/sh\x00'
+
+        Arguments:
+            `num`: the syscall number
+            `args`: arguments to the syscall
+            `ret`: Specifically use a 'syscall; ret' gadget for syscalls (instead of 'syscall')
+                `ret` WILL NOT WORK unless you have the dev verison of pwntools installed.
+        
+        Returns:
+            Nothing. Will raise errors if things go wrong.
         '''
         if context.arch != 'amd64':
             raise NotImplementedError("syscall_call() is only implemented for amd64 right now")
-        if self.syscall is None:
+        # get the syscall gadget
+        if ret:
+            if parse_version(PWNLIB_VER) < parse_version('4.4.0dev0'):
+                raise NotImplementedError('"syscall; ret" gadgets are only available on the '
+                'latest version of pwntools.')
+            syscall = self.find_gadget(['syscall', 'ret'])
+        else:
+            syscall = self.syscall
+        if syscall is None:
             raise AttributeError("ROP unable to find syscall gadget")
+        # write the rop chain
         self.pop({'rax': num})
-        self.raw(Call('syscall', self.syscall.address, args))
+        self.raw(Call('syscall', syscall.address, args))
     def pop(self, registers: dict):
         '''convinence function to edit a few registers
         >>> rop = ROP("./binary")

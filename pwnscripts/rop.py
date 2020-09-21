a9688b885b822f12ec464bf603d45f0cd7e4f0ec
152334H <54623771+152334H@users.noreply.github.com>
2020-09-21 20:12:30 +0800

    Add ROP.pop.<reg>

--- pwnscripts/rop.py
+++ pwnscripts/rop.py
@@ -4,20 +4,58 @@ from pwnlib import __version__ as PWNLIB_VER
 from pwnlib import rop
 from pwnlib.rop.call import Call
 from pwnlib.util.packing import flat
+from pwnlib.shellcraft import registers
 from pwnscripts.context import context
 class ROP(rop.rop.ROP):
-    def chain(self, base=None):
+    '''Extended pwnscripts ROP class
+    '''
+    class Pop():
+        '''Internal constructor for ROP.pop
+        This class is a wrapper, and should not be called outside of ROP().__init__().
+        It is publicly exposed to provide documentation under help() for viewing.
+        
+        Examples:
+        >>> rop = ROP("./binary")
+        >>> rop.pop({'rax':0x3b, 'rsi': 0, 'rdx': 0})
+        >>> print(rop.dump())
+        0x0000:         0x44a309 pop rdx; pop rsi; ret
+        0x0008:              0x0
+        0x0010:              0x0
+        0x0018:         0x41e4af pop rax; ret
+        0x0020:             0x3b
+        >>> rop = ROP('./binary')
+        >>> rop.pop.rdi(1)
+        >>> print(rop.dump())
+        0x0000:         0x401696 pop rdi; ret
+        0x0008:              0x1
+        '''
+        def __init__(self, rop):
+            self.rop = rop
+        def __call__(self, regs: dict) -> None:
+            '''convinence function to edit a few registers'''
+            self.rop._chain += [t[0] for t in self.rop.setRegisters(regs)]
+        def __getattr__(self, attr):
+            '''Allow for ROP.pop.<register> '''
+            if attr not in getattr(registers, context.arch):
+                raise AttributeError("module 'ROP.pop' has no attribute %r" % attr)
+            # else: Register is being set
+            return lambda v: self({attr:v})
+
+    def __init__(self, *a, **kw):
+        self.pop = ROP.Pop(self)
+        super().__init__(*a, **kw)
+    def chain(self, base=None) -> bytes:
         '''Build the ROP chain (and use a `base` if given)
 
         Returns:
             str containing raw ROP bytes
         '''
         return flat(self.build(base=base))
-    def dump(self, base=None):
+    def dump(self, base=None) -> str:
         '''Dump the ROP chain in an easy-to-read manner
         Optional `base` argument to change the ROP base'''
         return self.build(base=base).dump()
-    def system_call(self, num: int, args: list, ret: bool=False):
+    def system_call(self, num: int, args: list, ret: bool=False) -> None:
         '''Making system calls without the massive overhead of SIGROP
         >>> context.arch = 'amd64'
         >>> r = ROP('./binary')
@@ -55,17 +93,5 @@ class ROP(rop.rop.ROP):
         if syscall is None:
             raise AttributeError("ROP unable to find syscall gadget")
         # write the rop chain
-        self.pop({'rax': num})
+        self.pop.rax(num)
         self.raw(Call('syscall', syscall.address, args))
-    def pop(self, registers: dict):
-        '''convinence function to edit a few registers
-        >>> rop = ROP("./binary")
-        >>> rop.pop({'rax':0x3b, 'rsi': 0, 'rdx': 0})
-        >>> print(rop.dump())
-        0x0000:         0x44a309 pop rdx; pop rsi; ret
-        0x0008:              0x0
-        0x0010:              0x0
-        0x0018:         0x41e4af pop rax; ret
-        0x0020:             0x3b
-        '''
-        self._chain += [t[0] for t in self.setRegisters(registers)]
